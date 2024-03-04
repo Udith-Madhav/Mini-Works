970221f9d5c979cb15c5ac4c60ff44efc2351c60
Udith-Madhav <udithmadhav@gmail.com>
2024-03-04 16:34:48 +0530

    04/03/2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/bottom_navigation.dart';
+import 'package:flutter_application_2/circle%20avatar.dart';
 import 'package:flutter_application_2/floating%20action%20button.dart';
 import 'package:flutter_application_2/radio_button.dart';
 import 'package:flutter_application_2/validation.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  Floating() ,
+      home: Avatar() ,
     );
   }
 }
